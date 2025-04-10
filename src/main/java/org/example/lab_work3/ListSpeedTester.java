89cf9dd47fa81b06393be50ab987843ba62fb40f
Olya <lysachevaolga@gmail.com>
2025-04-10 18:02:49 +0300

    Add docs for ListSpeedTester

--- src/main/java/org/example/lab_work3/ListSpeedTester.java
+++ src/main/java/org/example/lab_work3/ListSpeedTester.java
@@ -3,10 +3,32 @@ package org.example.lab_work3;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+ * Класс для сравнительного тестирования производительности операций
+ * в ArrayList и LinkedList.
+ * <p>
+ * Класс выполняет замеры времени выполнения различных операций (добавление,
+ * удаление, получение элементов) на разных позициях в списках и выводит
+ * результаты сравнения в табличном формате.
+ */
 class ListSpeedTester {
+    /**
+     * Размеры тестовых выборок для проведения замеров производительности.
+     */
     private static final int[] SIZES = new int[]{1000, 2000, 4000, 5000, 6000, 7000, 8000};
+
+    /**
+     * Размер тестового списка, который будет использоваться для замеров.
+     */
     private static final int TEST_SIZE = 100000;
 
+    /**
+     * Запускает сравнительное тестирование ArrayList и LinkedList.
+     * <p>
+     * Метод инициализирует оба типа списков с тестовым количеством элементов,
+     * затем последовательно выполняет тесты для каждого размера операций из массива SIZES.
+     * </p>
+     */
     static void runComparison() {
         var arrayBased = new ArrayList<Integer>(TEST_SIZE);
         var nodeBased = new LinkedList<Integer>();
@@ -20,6 +42,13 @@ class ListSpeedTester {
         }
     }
 
+    /**
+     * Выполняет тестирование операций для двух реализаций списка.
+     *
+     * @param arrList  тестируемый ArrayList
+     * @param linkList тестируемый LinkedList
+     * @param opsCount количество операций для выполнения в каждом тесте
+     */
     private static void testOperations(ArrayList<Integer> arrList, LinkedList<Integer> linkList, int opsCount) {
         System.out.print("Operations count: " + opsCount + "\n");
         System.out.print("Operation          | ArrayList | LinkedList\n");
@@ -52,6 +81,13 @@ class ListSpeedTester {
                 timeTest(opsCount, linkList::getLast) + "\n");
     }
 
+    /**
+     * Выполняет переданное действие указанное количество раз и замеряет общее время выполнения.
+     *
+     * @param repeats количество повторений действия
+     * @param action  тестируемое действие
+     * @return общее время выполнения в наносекундах
+     */
     private static long timeTest(int repeats, Runnable action) {
         return measureDuration(() -> {
             for (int i = 0; i < repeats; ++i) {
@@ -60,6 +96,12 @@ class ListSpeedTester {
         });
     }
 
+    /**
+     * Замеряет время выполнения операции.
+     *
+     * @param operation операция, время выполнения которой нужно измерить
+     * @return время выполнения в наносекундах
+     */
     private static long measureDuration(Runnable operation) {
         long start = System.nanoTime();
         operation.run();
