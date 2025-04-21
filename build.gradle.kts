04b30b9615d34aa42fca4466e572c000dff510b5
Olya <lysachevaolga@gmail.com>
2025-04-21 17:13:10 +0300

    Add csv lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.opencsv:opencsv:5.5.2")
 }
 
 tasks.test {
